421c9fea933f52e4d4c332dfff5afaaf9f929c9b
sb8956 <65844764+sb8956@users.noreply.github.com>
2020-10-09 17:30:46 +0900

    Gridview

---
+++ app/src/main/java/com/example/teamproject/Gridview.java
@@ -0,0 +1,28 @@
+package com.example.teamproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class Gridview extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridview);
+
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(Gridview.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
