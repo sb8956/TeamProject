154696df55f7afd363b8029c7b08b4ae55c285fa
yoonh <sb_8956@naver.com>
2020-10-24 21:37:22 +0900

    menu

--- app/src/main/java/com/example/teamproject/MapsActivity.java
+++ app/src/main/java/com/example/teamproject/MapsActivity.java
@@ -1,19 +1,27 @@
 package com.example.teamproject;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
+import android.R.drawable;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.teamproject.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -22,17 +30,25 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CustomCap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.maps.model.SquareCap;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnPolylineClickListener,GoogleMap.OnPolygonClickListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnPolylineClickListener,GoogleMap.OnPolygonClickListener {
+
+
 
     // 권한 체크 요청 코드 정의
     public static final int REQUEST_CODE_PERMISSIONS = 1000;
@@ -47,6 +63,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        actionBar = getSupportActionBar();
+//        actionBar.setDisplayShowCustomEnabled(true);
+//        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+
 
         // GoogleAPIClient의 인스턴스 생성
         if (mGoogleApiClient == null) {
@@ -63,6 +87,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                //select logout item
+                break;
+            case R.id.account:
+                //select account item
+                break;
+            case android.R.id.home:
+                //select back button
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -91,10 +138,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
 
-        String[] xy = new String[]{"37.566535","126.97796919","37.221804","127.186695","37.220000","127.186666"};
+
+        String[] xy = new String[]{"37.221900","127.18800","37.221804","127.186695","37.220000","127.186666"};
 
         ArrayList<LatLng> loc=new ArrayList<LatLng>();
 
+        int count = 1;
         for (int i=0;i<xy.length;i++){
 
             double tmp = Double.parseDouble(xy[i]);
@@ -105,10 +154,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             loc.add(latLng);
 
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Pin"));
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Pin"+count).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_purple)));
+            count++;
         }
 
+
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(loc.get(loc.size()-1)));
+
 
 //        // 서울 위치
 //        LatLng seoul = new LatLng(37.566535, 126.97796919);
@@ -125,7 +179,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        mMap.addMarker(new MarkerOptions()
 //                .position(MJU2)
 //                .title("명지대2"));
-
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(MJU2));
 
         // 카메라 줌
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
@@ -141,14 +195,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
+
+        //arraylist 다시 배열에 넣는거 아직 않마 까먹ㅇ멋어ㅏ
+        LatLng[] line = {
+                loc.get(0),loc.get(1),loc.get(2)
+        };
         //좌표끼리 선 긋기 좌표가 추가 될때마다 새로운 선을 만들어야 하나.. 아니면 그냥 좌표하나씩 추가해야하나 고민
         //일단 좌표에 들어온 순서로 선이 그어짐 -> 시간별로 추가할수 있도록 해야 함
+
+
+        //좌표 두개마다 각각의 polyline을 생성해야 각각 화살표로 나올수 있음
+        //for문 사용해서 polyline 만들어보기
+//        for(int i=0; i<line.length;){
+//            Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+//                    .clickable(true)
+//                    .add(   line[i], line[++i]
+//                    ) .width(10)
+//
+//                    .geodesic(true));
+//
+//
+//        }
+
+
         Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
-                .add(   loc.get(0), loc.get(1), loc.get(2)
-                ) );
+                .add(   line
+                ) .width(10)
+
+                .geodesic(true));
+
+        polyline1.setEndCap(new CustomCap(BitmapDescriptorFactory.fromResource(R.drawable.ic_arrow), 15));
+        polyline1.setStartCap(new CustomCap(BitmapDescriptorFactory.fromResource(R.drawable.ic_circle), 15));
+
+
+
         googleMap.setOnPolylineClickListener(this);
         googleMap.setOnPolygonClickListener(this);
+
+
+
     }
 
     //두 좌표간 거리 구함 -> 어디다 쓰지? 아직 안씀
@@ -176,7 +262,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         switch (requestCode) {
             case REQUEST_CODE_PERMISSIONS:
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "권한 체크 거부 됨", Toast.LENGTH_SHORT).show();
                 }
                 return;
@@ -197,7 +283,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void onLastLocationButtonClicked(View view) {
         // 권한 체크
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSIONS);
             return;
         }
